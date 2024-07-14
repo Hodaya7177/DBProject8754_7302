16666eba66347a924a0633215b0cab20346d4d3c
Hodaya Ashkenazi <114957298+Hodaya7177@users.noreply.github.com>
2024-07-14 15:07:47 +0300

    Update SumOfDonorsForEvent.sql

--- שלב 3/תוכנית שניה /SumOfDonorsForEvent.sql
+++ שלב 3/תוכנית שניה /SumOfDonorsForEvent.sql
@@ -1,14 +1,14 @@
-CREATE OR REPLACE FUNCTION SumOfDonorsForEvent(p_EventID IN NUMBER) RETURN NUMBER IS
+CREATE OR REPLACE FUNCTION SumOfDonorsForEvent(p_event_id IN NUMBER)
+RETURN NUMBER
+IS
     v_sum_of_donors NUMBER;
 BEGIN
-    -- Function to count the number of distinct donors for a given event ID
-    SELECT COUNT(DISTINCT DonorID_) INTO v_sum_of_donors
-    FROM Event
-    WHERE EventID = p_EventID;
+    SELECT COUNT(DISTINCT d.donorid)
+    INTO v_sum_of_donors
+    FROM System.Donation dn
+    JOIN System.Participates p ON dn.donorid = p.donorid
+    WHERE p.eventid = p_event_id;
     
     RETURN v_sum_of_donors;
-EXCEPTION
-    -- Exception handling: if any error occurs, return 0
-    WHEN OTHERS THEN
-        RETURN 0;
-END SumOfDonorsForEvent;
+END;
+/

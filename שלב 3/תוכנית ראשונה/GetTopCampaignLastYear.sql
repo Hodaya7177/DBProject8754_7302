58509630fd55645bef0778c8aa3d0fe60f268ce6
Hodaya Ashkenazi <114957298+Hodaya7177@users.noreply.github.com>
2024-07-14 03:00:37 +0300

    Update GetTopCampaignLastYear.sql

--- שלב 3/תוכנית ראשונה/GetTopCampaignLastYear.sql
+++ שלב 3/תוכנית ראשונה/GetTopCampaignLastYear.sql
@@ -1,1 +1,21 @@
+CREATE OR REPLACE FUNCTION GetTopCampaignLastYear
+RETURN NUMBER
+IS
+    v_max_donation NUMBER := 0;
+BEGIN
+    SELECT MAX(total_donations) INTO v_max_donation
+    FROM (
+        SELECT SUM(d.amount) AS total_donations
+        FROM System.Campaign c
+        JOIN System.Donation d ON c.campaignid = d.campaignid
+        WHERE EXTRACT(YEAR FROM c.enddate) = EXTRACT(YEAR FROM SYSDATE) - 1
+        GROUP BY c.campaignid
+    );
 
+    RETURN v_max_donation;
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        RETURN 0;
+    WHEN OTHERS THEN
+        RAISE;
+END;
